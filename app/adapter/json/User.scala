60f2fd019c20458c4246abdc37aa7779ba1574d6
okoeee <yk27.puxcm@gmail.com>
2024-10-27 14:50:00 +0900

    regenerate model

--- app/adapter/json/User.scala
+++ app/adapter/json/User.scala
@@ -5,14 +5,13 @@ import play.api.libs.json._
 /**
   * Represents the Swagger definition for User.
   * @param id ユーザーID
-  * @param name ユーザー名
   * @param email メールアドレス
   * @param age 年齢
   */
 case class User(
-  id: Option[Long],
-  name: Option[String],
-  email: Option[String],
+  id: Long,
+  name: UserName,
+  email: String,
   age: Option[Int]
 )
 
